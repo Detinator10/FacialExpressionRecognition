cf39fb23efc2791250e9380b01547820bef9d360
Detlef K <detlefk@archanics.com>
2020-08-13 12:51:04 -0700

    Adding image augmentation

--- gui2.py
+++ gui2.py
@@ -33,32 +33,3 @@ cv2.ocl.setUseOpenCL(False)
 
 emotion_dict = {0: "   Angry   ", 1: "Disgusted", 2: "  Fearful  ", 3: "   Happy   ", 4: "  Neutral  ",
                 5: "    Sad    ", 6: "Surprised"}
-cap = cv2.VideoCapture(0)
-
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, (600, 500))
-    bounding_box = cv2.CascadeClassifier(
-        './venv/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
-
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    num_faces = bounding_box.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-    for (x, y, w, h) in num_faces:
-        cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
-        roi_gray_frame = gray[y:y + h, x:x + w]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
-        prediction = emotion_model.predict(cropped_img)
-
-        maxindex = int(np.argmax(prediction))
-        print(emotion_dict[maxindex])
-        cv2.putText(gray, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-    # Display the resulting frame
-    cv2.imshow('frame', gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
